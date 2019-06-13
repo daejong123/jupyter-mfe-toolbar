8a7f0fc6a7ebaad95b5d960c8a0166778b8f12a5
lindazhong <daejong2014@163.com>
2019-06-13 13:23:07 +0800

    fix bug

--- lib/MfeEWidget.d.ts
+++ lib/MfeEWidget.d.ts
@@ -2,9 +2,9 @@ import { Widget } from '@phosphor/widgets';
 import { ToolbarButton } from '@jupyterlab/apputils';
 import { NotebookPanel } from '@jupyterlab/notebook';
 export default class MfeMenuWidget extends Widget {
-    toolbarBtns: ToolbarButton[];
-    panel: NotebookPanel;
-    constructor(panel: any);
-    addBtn(component: any): void;
-    render(): void;
+    private toolbarBtns;
+    private panel;
+    constructor(panel: NotebookPanel);
+    addBtn(component: ToolbarButton): void;
+    private render;
 }
